a808be23c0d06023439a1c2fc8fcc968f5766d02
subhashdamodaran <subhashdamodaran@gmail.com>
2023-08-09 10:50:00 +0530

    Update options.js

--- options.js
+++ options.js
@@ -1,18 +1,24 @@
 function saveOptions() {
-    const targetFolderId = document.getElementById('targetFolderIdInput').value;
-    chrome.storage.sync.set({ targetFolderId: targetFolderId }, () => {
-      console.log('Options saved:', targetFolderId);
-    });
-  }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    chrome.storage.sync.get(['targetFolderId'], (result) => {
-      if (result.targetFolderId) {
-        document.getElementById('targetFolderIdInput').value = result.targetFolderId;
-      }
-    });
-  
-    const saveButton = document.getElementById('saveButton');
-    saveButton.addEventListener('click', saveOptions);
+  // Use chrome.permissions API to request bookmarks permission before storing the folder ID
+  chrome.permissions.request({ permissions: ['bookmarks'] }, (granted) => {
+    if (granted) {
+      const targetFolderId = document.getElementById('targetFolderIdInput').value;
+      chrome.storage.sync.set({ targetFolderId: targetFolderId }, () => {
+        console.log('Options saved:', targetFolderId);
+      });
+    } else {
+      console.warn('Permission denied for accessing bookmarks.');
+    }
   });
-  
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.sync.get(['targetFolderId'], (result) => {
+    if (result.targetFolderId) {
+      document.getElementById('targetFolderIdInput').value = result.targetFolderId;
+    }
+  });
+
+  const saveButton = document.getElementById('saveButton');
+  saveButton.addEventListener('click', saveOptions);
+});
